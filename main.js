5ca6ee78afb03a5e8099df757f5fffa04520ce08
Thomas_Thabethe <thomasthabethe3@gmail.com>
2023-02-01 08:11:50 +0200

    Done

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ let weather  = {
         const { icon, description } = data.weather[0];
         const{ temp, humidity} = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed)
         document.querySelector(".city").innerText = " Weather in " + name;
         document.querySelector(".icon").src = 
         "https://openweathermap.org/img/wn/"+icon+".png"
@@ -22,6 +21,8 @@ let weather  = {
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed:" + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+name+"')"
 
     },
     search: function () {
@@ -31,4 +32,13 @@ let weather  = {
 
 document.querySelector(".search button").addEventListener("click",function () {
     weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+
+    if(event.key == "Enter"){
+        weather.search();
+    }
 })
+
+weather.fetchWeather("Durban");
